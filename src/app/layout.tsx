966d7511afe4fa719c5d755c4c2e49b1eb0454ce
Hakko <67168651+HakkoDev@users.noreply.github.com>
2025-09-04 23:40:48 -0600

    Add live chat widget

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -84,6 +84,25 @@ export default function Layout({ children }: { children: ReactNode }) {
                         `,
                     }}
                 />
+
+
+                <link rel="preconnect" href="https://chat.prism-host.com">
+                <script>
+                (function(d,t) {
+                    var BASE_URL="https://chat.prism-host.com";
+                    var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+                    g.src=BASE_URL+"/packs/js/sdk.js";
+                    g.defer = true;
+                    g.async = true;
+                    s.parentNode.insertBefore(g,s);
+                    g.onload=function(){
+                    window.chatwootSDK.run({
+                        websiteToken: 'QV1vpqzZAZwAZdvGUcbLPdtW',
+                        baseUrl: BASE_URL
+                    })
+                    }
+                })(document,"script");
+                </script>
             </body>
         </html>
     );
